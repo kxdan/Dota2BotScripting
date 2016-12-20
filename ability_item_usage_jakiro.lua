93f7d245184f4f9549c938d513e5b27d047d64f0
kxdan <dm00247@surrey.ac.uk>
2016-12-20 13:32:31 +0000

    Added Simple Jakiro Bot

---
+++ ability_item_usage_jakiro.lua
@@ -0,0 +1,24 @@
+
+require( GetScriptDirectory().."/ability_item_usage_generic" )
+
+----------------------------------------------------------------------------------------------------
+
+function AbilityUsageThink()
+
+	--print( "Jakiro.AbilityUsageThink" );
+
+	ability_item_usage_generic.AbilityUsageThink();
+
+end
+
+----------------------------------------------------------------------------------------------------
+
+function ItemUsageThink()
+
+	--print( "Jakiro.ItemUsageThink" );
+
+	ability_item_usage_generic.ItemUsageThink();
+
+end
+
+----------------------------------------------------------------------------------------------------
